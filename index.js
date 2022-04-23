d6a6a45f9e033197267d775601f0775b22a9406f
Gabriel-PF <fsp.gabriel@gmailcom>
2022-04-23 08:45:32 +0200

    final

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => {
+      return a-b
+    })
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if(!this.items[pos]){
+    throw new Error("OutOfBounds");
+  }
+  return this.items[pos]
+}
 
-  min() {}
+  max() {
+    
+    if(this.items.length == 0){
+    throw new Error("EmptySortedList")
+  }
+  return this.items.pop();}
 
-  sum() {}
+  min() {
+    
+    if (this.items.length == 0){
+    throw new Error("EmptySortedList")
+  }
+  return this.items.shift();}
+
+  sum() {
+    
+    let aggregateSum = 0;
+    this.items.forEach((element)=>{
+      aggregateSum += element
+    });
+    return aggregateSum
+  }
 
   avg() {}
 }
